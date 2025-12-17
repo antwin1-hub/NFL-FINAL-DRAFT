f4df58f5f59e323cfc3787f086ea6545157383ba
antwin1-hub <antwin1@tcnj.edu>
2025-12-17 01:52:11 -0500

    Update antwin1-final_project.R

--- antwin1-final_project.R
+++ antwin1-final_project.R
@@ -1,17 +1,73 @@
-# load the data
+############################################################
+# antwin1-final_project.R
+# Final Project Analysis Script
+############################################################
+
+############################
+# 1) Load packages (only if taught)
+############################
 library(readr)
+library(dplyr)
+
+############################
+# 2) Load data as "data"
+# (Make sure data.csv is in the same folder as this script)
+############################
 data <- read_csv("data.csv")
 
-# descriptive statistics
-summary(data$total_penalties)
-sd(data$total_penalties)
-summary(data$rushing_first_downs)
-sd(data$rushing_first_downs)
+############################
+# 3) Complete-case dataset
+############################
+data_complete <- data %>%
+  filter(complete.cases(.))
 
-# scatter plot
-plot(data$rushing_first_downs, data$total_penalties)
+############################
+# 4) Descriptive statistics (Table 1)
+############################
+summary(data_complete$total_penalties)
+sd(data_complete$total_penalties)
 
+summary(data_complete$rushing_first_downs)
+sd(data_complete$rushing_first_downs)
 
-# linear regression
-lm <- lm(rushing_first_downs ~ total_penalties, data = data)
-summary(lm)
+############################
+# 5) Scatterplot (Figure 1)
+############################
+plot(
+  data_complete$total_penalties,
+  data_complete$rushing_first_downs,
+  xlab = "Total Penalties",
+  ylab = "Rushing First Downs",
+  main = "Rushing First Downs vs Total Penalties"
+)
+
+############################
+# 6) Linear regression (statistical test)
+############################
+model <- lm(rushing_first_downs ~ total_penalties, data = data_complete)
+summary(model)
+
+# Add regression line
+abline(model)
+
+############################
+# 7) Assumptions check (Residual Plot)
+############################
+plot(
+  resid(model),
+  xlab = "Observation",
+  ylab = "Residual",
+  main = "Residual Plot"
+)
+abline(h = 0)
+
+# Scatter plot: Total Penalties vs Rushing First Downs
+
+plot(
+  data_complete$total_penalties,
+  data_complete$rushing_first_downs,
+  xlab = "Total Penalties",
+  ylab = "Rushing First Downs",
+  main = "Scatterplot of Rushing First Downs by Total Penalties"
+)
+
